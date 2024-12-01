8d156fd754a0b6211b24dfbbdf8afccc4f4138e7
Hans-J. Schmid <knock@myopendoor.de>
2024-12-01 08:20:15 +0100

    init
    dummy change

---
+++ exercises/18_iterators/iterators3.rs
@@ -0,0 +1,75 @@
+#[derive(Debug, PartialEq, Eq)]
+enum DivisionError {
+    DivideByZero,    // Example: 42 / 0
+    IntegerOverflow, // Only case for `i64`: `i64::MIN / -1` because the result is `i64::MAX + 1`
+    NotDivisible,    // Example: 5 / 2 = 2.5
+}
+
+// TODO: Calculate `a` divided by `b` if `a` is evenly divisible by `b`.
+// Otherwise, return a suitable error.
+fn divide(a: i64, b: i64) -> Result<i64, DivisionError> {
+    match b {
+        0                             => Err(DivisionError::DivideByZero),
+        _ if a == i64::MIN && b == -1 => Err(DivisionError::IntegerOverflow),
+        _ if a % b != 0               => Err(DivisionError::NotDivisible),
+        _                             => Ok(a / b)
+    }
+}
+
+// TODO: Add the correct return type and complete the function body.
+// Desired output: `Ok([1, 11, 1426, 3])`
+fn result_with_list() -> Result<Vec<i64>, DivisionError> {
+    let numbers = [27, 297, 38502, 81];
+    numbers.into_iter().map(|n| divide(n, 27)).collect()
+}
+
+// TODO: Add the correct return type and complete the function body.
+// Desired output: `[Ok(1), Ok(11), Ok(1426), Ok(3)]`
+fn list_of_results() -> Vec<Result<i64, DivisionError>> {
+    let numbers = [27, 297, 38502, 81];
+    numbers.into_iter().map(|n| divide(n, 27)).collect()
+}
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_success() {
+        assert_eq!(divide(81, 9), Ok(9));
+    }
+
+    #[test]
+    fn test_divide_by_0() {
+        assert_eq!(divide(81, 0), Err(DivisionError::DivideByZero));
+    }
+
+    #[test]
+    fn test_integer_overflow() {
+        assert_eq!(divide(i64::MIN, -1), Err(DivisionError::IntegerOverflow));
+    }
+
+    #[test]
+    fn test_not_divisible() {
+        assert_eq!(divide(81, 6), Err(DivisionError::NotDivisible));
+    }
+
+    #[test]
+    fn test_divide_0_by_something() {
+        assert_eq!(divide(0, 81), Ok(0));
+    }
+
+    #[test]
+    fn test_result_with_list() {
+        assert_eq!(result_with_list().unwrap(), [1, 11, 1426, 3]);
+    }
+
+    #[test]
+    fn test_list_of_results() {
+        assert_eq!(list_of_results(), [Ok(1), Ok(11), Ok(1426), Ok(3)]);
+    }
+}
